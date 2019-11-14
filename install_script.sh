cbcc1e0e2856be0cefe469c8ca003512b082021e
Arnout Devos <arnoutdev@gmail.com>
2019-11-14 15:24:02 +0100

    Removed need to input y (yes) to install

--- install_script.sh
+++ install_script.sh
@@ -11,13 +11,13 @@ conda create -n my_env python=3.6
 conda activate my_env
 
 # Install packages
-conda install nb_conda # to see my_env inside jupyter lab/notebook
-conda install matplotlib
-conda install scipy
+conda install -y nb_conda # to see my_env inside jupyter lab/notebook
+conda install -y matplotlib
+conda install -y scipy
 
 python -m ipykernel install --user --name myenv --display-name "Python (myenv)"
 
-sudo apt install nvidia-410 # to install cuda 10.0
+#sudo apt install nvidia-410 # to install cuda 10.0 --> we have not control over driver, that's Kubernetes' decision
 conda uninstall tensorflow
-conda install -n my_env tensorflow-gpu==1.14.0 # install tensorflow 1.14, which is tested with cuda 10
+conda install -y -n my_env tensorflow-gpu==1.14.0 # install tensorflow 1.14, which is tested with cuda 10
 jupyter lab --ip=0.0.0.0 --port=8888 --allow-root &
