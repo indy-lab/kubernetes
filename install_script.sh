1b7bbc8133ebb3b66fc0e9db486f33771bc2b185
Arnout Devos <arnoutdev@gmail.com>
2019-11-24 12:12:07 +0100

    Updated install script with explicit package versions

--- install_script.sh
+++ install_script.sh
@@ -6,18 +6,21 @@ sudo sh Anaconda3-2019.07-Linux-x86_64.sh
 # Source newly created conda command
 source /root/anaconda3/etc/profile.d/conda.sh
 
-# Create virtual environment my_env
+# Create virtual environment 'my_env'
 conda create -n my_env python=3.6
 conda activate my_env
 
-# Install packages
+# Install Jupyter plugin and general Python packages
 conda install -y nb_conda # to see my_env inside jupyter lab/notebook
 conda install -y matplotlib
 conda install -y scipy
 
 python -m ipykernel install --user --name myenv --display-name "Python (myenv)"
 
-#sudo apt install nvidia-410 # to install cuda 10.0 --> we have not control over driver, that's Kubernetes' decision
+#Install TensorFlow-gpu 1.14 and PyTorch
 conda uninstall tensorflow
-conda install -y -n my_env tensorflow-gpu==1.14.0 # install tensorflow 1.14, which is tested with cuda 10
+conda install -y -n my_env tensorflow-gpu=1.14.0 # install tensorflow 1.14, which is tested with cuda 10
+conda install -y -n my_env pytorch=1.3.1 torchvision=0.4.2 cudatoolkit=10.1 -c pytorch
+
+# Start Jupyter Lab server
 jupyter lab --ip=0.0.0.0 --port=8888 --allow-root &
